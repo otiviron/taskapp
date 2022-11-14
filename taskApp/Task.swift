8e798995acbe9b3ce4ca04358e67eafa082b75dc
takahiro.koizumi <ktakahiro091@icloud.com>
2022-11-14 16:23:20 +0900

    taskapp提出

---
+++ taskApp/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  taskApp
+//
+//  Created by Takahiro Koizumi on 2022/11/13.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+
+    // タイトル
+    @objc dynamic var title = ""
+
+    // 内容
+    @objc dynamic var contents = ""
+
+    // 日時
+    @objc dynamic var date = Date()
+    
+    //カテゴリ
+    @objc dynamic var category = ""
+
+    // id をプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
